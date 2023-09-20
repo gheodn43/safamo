a4b2e8b787003a4fa0d921862e07c3dc118bfe24
WED201C <pnamhuynhle@gmail.com>
2023-09-20 09:55:55 +0700

    newcommit

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,130 +1,45 @@
 import React, { Component } from 'react';
-import {Link} from "react-router-dom";
-import axios from 'axios';
+import { Link } from 'react-router-dom';
+
 class Login extends Component {
-    constructor(props) {
-        super(props);
-        this.state={
-            username:'',
-            password:'',
-            message:'',
-            result:'',
-        } 
-        this.handleChangeField=this.handleChangeField.bind(this);
-        this.handleSubmit= this.handleSubmit.bind(this);
-        this.handleClickClose=this.handleClickClose.bind(this);
-    }
-    handleChangeField(){
-        this.setState({
-           username:this.refs.username.value,
-           password:this.refs.password.value
-       })
-    }
-    handleSubmit(){
-        let {username,password}= this.state;
-        if(!username || !password)
-        {
-            this.setState({
-                message:"Vui lòng nhập đầy đủ thông tin!!"
-            })
-        }else{
-            axios.post('/nguoi-dung/dang-nhap',{
-                username:username,
-                password:password
-             },{headers: {'Accept': 'application/json'}})
-            .then(res => {
-                this.setState({
-                    message:res.data.message,
-                    result:res.data.result,
-                    
-                });
-                if(this.state.result){
-                    const close = document.getElementById("closelogin");
-                    close.click();
-                    this.props.callApiGetUser();
-                    this.handleClickClose();
-                    //this.props.onCloseForm(true);
-                }
-            })
-            .catch( (error) => console.log(error));  
-        }
-        
-    }
-    handleClickClose(){
-        this.setState({
-                username:'',
-                password:'',
-                message:'',
-        });
-    }
-    render() {
-       
-        var KTL=true;
-        if(this.state.message.length>0){
-            KTL=false;
-        }
-        return (
-        <div className="modal fade" id="modalLoginForm" tabIndex={-1} role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-            <div className="modal-dialog" role="document">
-                <div className="modal-content">
-                    <div className="modal-header text-center">
-                    <h4 className="modal-title w-100 font-weight-bold">Đăng nhập</h4>
-                    <button type="button" className="close" id="closelogin" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true" onClick={this.handleClickClose}>×</span>
-                    </button>
-                    </div>
-                    <div className="modal-body mx-3">
-                     {!KTL && 
-                          <p  className="error_message-login">{ this.state.message} 
-                          </p>
-                     }
-                    <div className="md-form mb-4 row">
-                        <div className="col-md-2 col-sm-2 col-xs-3 icon_username">
-                        {/* <img src={img_icon_login} alt="icon"/> */}
-                        </div>
-                        <div className="col-md-10 col-sm-8 col-xs-9 inputusername">
-                                <input type="text" 
-                                className="form-control " 
-                                placeholder="Tên đăng nhập"
-                                ref="username"
-                                onChange={this.handleChangeField}
-                                value={this.state.username}/>
-                        </div>
-                    </div>
-                    <div className="md-form mb-3 row">
-                        <div className="col-md-2 col-sm-2 col-xs-3 icon_password">
-                        {/* <img src={img_icon_password} alt="icon"/> */}
-                        </div>
-                        <div className="col-md-10 col-sm-8 col-xs-9 inputpassword">
-                                <input type="password" 
-                                id="defaultForm-pass" 
-                                className="form-control " 
-                                placeholder="Mật khẩu"
-                                ref="password"
-                                onChange={this.handleChangeField}
-                                value={this.state.password}/>
-                        </div>
-                        <div className="col-md-4 col-sm-4 col-xs-4 span_registerinlogin">
-                                <Link to='/nguoi-dung/dang-ky' data-dismiss="modal" aria-label="Close" data-toggle="modal" data-target="#modalRegisterForm">
-                                Quên mật khẩu
-                                </Link>
-                        </div>
-                        <div className="col-md-8 col-sm-8 col-xs-8 span_registerinlogin">
-                                <span>Bạn chưa có tài khoản?<Link to='/nguoi-dung/dang-ky' data-dismiss="modal" aria-label="Close" data-toggle="modal" data-target="#modalRegisterForm">
-                                Đăng ký
-                                </Link></span>
-                        </div>
-                        
-                    </div>
-                    </div>
-                    <div className="modal-footer d-flex justify-content-center bntdangnhap">
-                        <input type="button" className="btn btn-default" value="Đăng nhập" onClick={this.handleSubmit}/>
-                    </div>
-                </div>
-            </div>
+  handleLogin = () => {
+    // Xử lý đăng nhập
+  };
+
+  render() {
+    return (
+      <div className="login-container">
+        <div className="login-form">
+          <h2>Đăng nhập</h2>
+
+          {/* Form đăng nhập */}
+          <form>
+            <input type="text" placeholder="Tên đăng nhập" />
+            <input type="password" placeholder="Mật khẩu" />
+            <button onClick={this.handleLogin}>Đăng nhập</button>
+          </form>
+
+          {/* Đăng kí */}
+          <p>
+            Bạn chưa có tài khoản?{' '}
+            <Link to="/register" className="link">
+              Đăng kí ngay
+            </Link>
+          </p>
+
+          {/* Đăng nhập với Facebook */}
+          <button className="facebook-login">
+            <i className="fab fa-facebook"></i> Đăng nhập với Facebook
+          </button>
+
+          {/* Đăng nhập với Gmail */}
+          <button className="gmail-login">
+            <i className="fab fa-google"></i> Đăng nhập với Gmail
+          </button>
         </div>
-        );
-    }
+      </div>
+    );
+  }
 }
 
 export default Login;
--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,28 +1,27 @@
 import React from 'react';
 import './style.css';
-import FacebookLoginComponent from "./FacebookLogin.js";
-import logo from './img/logo.png'; // Import hình ảnh
-const Navbar = () => {
+import logo from './img/logo.png';
+import { Link } from 'react-router-dom';
+import './Login.js';
+
+const Navbar = ({ isLoggedIn }) => {
+  const handleLogoClick = () => {
+    // Thực hiện chuyển hướng về trang home khi nhấn vào logo
+    window.location.href = 'http://localhost:3000/'; // Thay '#home' bằng đường dẫn trang home của bạn
+  };
+
   return (
     <div className="navbar">
       <a href="#rent">Rent</a>
       <a href="#shared-accommodation">Shared Accommodation</a>
       <a href="#agent-finder">Agent Finder</a>
-      <div className="logo">
-        <img src={logo} alt="Logo" /> {/* Sử dụng biến chứa đường dẫn hình ảnh */}
+      <div className="logo" onClick={handleLogoClick}>
+        <img src={logo} alt="Logo" />
       </div>
       <a href="#manage-rentals">Manage Rentals</a>
-      <a href="#profile">Profile</a>
-      <div className="dropdown">
-        <button className="dropbtn">Tài khoản</button>
-        <div className="dropdown-content">
-          <a href="#profile">Profile</a>
-          <a href="#settings">Settings</a>
-          <a href="#login"><FacebookLoginComponent/></a>
-          <a href="#guess">Guess</a>
-          <a href="#logout">Logout</a>
-        </div>
-      </div>
+      <a href="#favourite">Favourite</a>
+      <a href="#sign-in">Sign in</a>
+      {/* <Link to="./Login.js">Sign In</Link> Chuyển đến trang đăng nhập */}
     </div>
   );
 };
